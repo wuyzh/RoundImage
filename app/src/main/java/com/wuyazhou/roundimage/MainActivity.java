6549a73121cec488e6472b8bd0327d9c8f593577
wuyazhou <wuyazhou-ds@gome.com.cn>
2019-06-10 11:54:06 +0800

    圆角Image

--- app/src/main/java/com/wuyazhou/roundimage/MainActivity.java
+++ app/src/main/java/com/wuyazhou/roundimage/MainActivity.java
@@ -1,13 +1,50 @@
 package com.wuyazhou.roundimage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.wuyazhou.roundimage.RoundImage.RoundedImageView;
 
+
+public class MainActivity extends Activity {
+
+    private RoundedImageView mRoundedImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRoundedImageView = (RoundedImageView) findViewById(R.id.round_image);
+
+        if (true){
+            //设置边角弧度
+            mRoundedImageView.setCornerRadius(20);
+            //设置为圆形
+            mRoundedImageView.setOval(true);
+
+            if (true){
+                //设置周围边框
+                mRoundedImageView.setBorderWidth(R.dimen.tab_user_border_width);
+                mRoundedImageView.setBorderColor(0xff00008b);
+            }
+            if (false) {
+                //换图片啊
+                //mRoundedImageView.setImageResource(R.color.clear);
+            } else {
+                //网络设置图片
+                mRoundedImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            }
+        }else {
+            if (true) {
+                mRoundedImageView.setBorderColor(0xff00008b);
+                mRoundedImageView.setBorderWidth(R.dimen.tab_user_border_width);
+                mRoundedImageView.setOval(false);
+                mRoundedImageView.setCornerRadius(0);
+                mRoundedImageView.setImageResource(R.mipmap.ic_user_normal_s);
+            } else {
+                mRoundedImageView.setImageResource(R.mipmap.ic_user_normal_s);
+            }
+        }
     }
 }
